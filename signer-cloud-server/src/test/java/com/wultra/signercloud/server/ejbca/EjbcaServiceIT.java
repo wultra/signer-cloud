4b8b3baf0d52eab106d2cf70e1d8ee59d5f099e3
Lubos Racansky <lubos.racansky@gmail.com>
2025-08-08 11:00:49 +0200

    Draft of EjbcaServiceIT

--- signer-cloud-server/src/test/java/com/wultra/signercloud/server/ejbca/EjbcaServiceIT.java
+++ signer-cloud-server/src/test/java/com/wultra/signercloud/server/ejbca/EjbcaServiceIT.java
@@ -18,8 +18,11 @@
 package com.wultra.signercloud.server.ejbca;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-import static org.assertj.core.api.Assertions.fail;
+import java.security.cert.X509Certificate;
 
 /**
  * Integration test for {@link EjbcaService}.
@@ -28,11 +31,37 @@ import static org.assertj.core.api.Assertions.fail;
  *
  * @author Lubos Racansky, lubos.racansky@wultra.com
  */
+@SpringBootTest
+@ActiveProfiles("dev")
 class EjbcaServiceIT {
 
+    @Autowired
+    private EjbcaService ejbcaService;
+
     @Test
-    void testEnrollCertificate() {
-        // TODO (racansky, 2025-08-08) implement integration tests
-        fail("todo");
+    void testEnrollCertificate() throws Exception {
+        final String csr = """
+                -----BEGIN CERTIFICATE REQUEST-----
+                MIICrjCCAZYCAQAwaTELMAkGA1UEBhMCQ1MxGzAZBgNVBAgMElRoZSBDemVjaCBS
+                ZXB1YmxpYzEPMA0GA1UEBwwGUHJhZ3VlMRUwEwYDVQQKDAxXdWx0cmEsIEluYy4x
+                FTATBgNVBAMMDCoud3VsdHJhLmNvbTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCC
+                AQoCggEBAKPiSmc+mzn9SCf7G84WBK6MmPxPlDT38cL8hWNkYGp7p8sxb+Kynx/G
+                bzEhvscLsuxlawQ/5hyDAO2Pg9860fo02lLZxOAfme9W6IYH/ZIke4sEly670WIp
+                cCXRbAwMc6GFwcIxyNywrvgwwcMmhJKcysu0sZ0RpB1j2fRfmes4LedlUqfb/R4w
+                pyZRIp5dW9kOT3v1mHaGuMy9iRSB8QRRHsoSuNX8HxAKzP80IRlbs4Kn1SBcpQKI
+                U6Y4R4YwkLas83JS+OMeLTDBeAWVAGsCGARc3Z9io39K66QNDwwgxAeqUWF5xB9H
+                XSHyPNxl072lddjiQ4S6YWMVwlgZWjcCAwEAAaAAMA0GCSqGSIb3DQEBDQUAA4IB
+                AQAwUJE9g0huEeW9QFAFQ9bBOPSwnCFTdbCua06xJIOH073iPwHQQ3ECIelaoOHS
+                poyWTNl/aumpN0D1qjM9OHxBv8te9aAHweWOjG4fdm/S3Qyhx/zTap0tLBWBolPI
+                7QbrA8pH5gpoMwTsT267hyYbQEaXyxn/ywK6Ev2etG4l7xh6aI4WDkwbgGzgTHwZ
+                uba+K2iCssb3jK4kwoPF4WARmCwCs1g9+Q+HNN5GnEnhgHN9Uc2q4JZgx7/+jkja
+                Pt0NeqcctnwtR+DbziukYzGdgJabFSXpQkRRf6vhgVm/350QXjFLpfBkEDKJtiS8
+                cAuJQRzaWMiNnSXwsFSNvuQh
+                -----END CERTIFICATE REQUEST-----
+                """;
+
+        final X509Certificate x509Certificate = ejbcaService.enrollCertificate(csr);
+        System.out.println(x509Certificate);
+        // TODO (racansky, 2025-08-08) add assertions and client certificate configuration
     }
 }
