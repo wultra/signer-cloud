b56ba39685159122cc47bbf146898c8017cc30e9
Lubos Racansky <lubos.racansky@gmail.com>
2025-08-13 10:51:27 +0200

    Fix #24: Create Docker

---
+++ deploy/docker-entrypoint.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+liquibase --headless=true --log-level=INFO --changeLogFile=$LB_HOME/db/changelog/db.changelog-master.xml \
+  --username="${DB_USERNAME:-}" \
+  --password="${DB_PASSWORD:-}" \
+  --url="${DB_URL}" \
+  update
+
+java ${JAVA_OPTS:-} -jar signer-cloud-server.war
