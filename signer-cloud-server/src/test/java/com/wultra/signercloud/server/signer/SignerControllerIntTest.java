de2b001f06d82adf391575551e36d57fe78ff29c
Lubos Racansky <lubos.racansky@gmail.com>
2025-08-19 11:23:24 +0200

    Fix test

--- signer-cloud-server/src/test/java/com/wultra/signercloud/server/signer/SignerControllerIntTest.java
+++ signer-cloud-server/src/test/java/com/wultra/signercloud/server/signer/SignerControllerIntTest.java
@@ -27,6 +27,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
@@ -53,6 +54,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 })
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
+@WithMockUser
 class SignerControllerIntTest {
 
     private static final String CREATE_UPDATE_SIGNER_ENDPOINT = "/api/signers";
