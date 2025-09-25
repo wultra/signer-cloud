8caf84608a192dd480193dbeb48f89aef9987e91
michal-rozehnal-w <michal.rozehnal@wultra.com>
2025-09-25 08:17:29 +0200

    Add missing Revocation reason enum

---
+++ signer-cloud-server/src/main/java/com/wultra/signercloud/server/signer/RevocationReason.java
@@ -0,0 +1,37 @@
+/*
+ * Signer Cloud
+ * Copyright (C) 2025 Wultra s.r.o.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.wultra.signercloud.server.signer;
+
+/**
+ * Reason of certificate revocation supported by EJBCA.
+ *
+ * @author Michal Rozehnal, michal.rozehnal@wultra.com
+ */
+public enum RevocationReason {
+    NOT_REVOKED,
+    UNSPECIFIED,
+    KEY_COMPROMISE,
+    CA_COMPROMISE,
+    AFFILIATION_CHANGED,
+    SUPERSEDED,
+    CESSATION_OF_OPERATION,
+    CERTIFICATE_HOLD,
+    REMOVE_FROM_CRL,
+    PRIVILEGES_WITHDRAWN,
+    AA_COMPROMISE
+}
